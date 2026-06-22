49a21b381878b15fbfd4d32c0e4a3b404d2bcbfe
Parth Kotwe <parthkotwe73@gmail.com>
2026-06-22 16:25:16 +0530

    Time: 0 ms (100%), Space: 35.8 MB (55.5%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        int start = 0;
-        int end = n-1;
-
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(nums[mid] < nums[mid+1]){
-                start = mid + 1;
-            }
-            else{
-                end = mid;
-            }
+        int totalsum=0;
+        int leftsum=0;
+        for(int i=0;i<nums.size();i++){
+            totalsum+=nums[i];
         }
-        return start;
+        for(int i=0;i<nums.size();i++){
+            if(leftsum == totalsum - leftsum - nums[i]){
+                return i;
+            }
+            leftsum += nums[i];
+        }
+        return -1;
     }
 };
